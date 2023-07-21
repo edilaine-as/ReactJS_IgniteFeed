d64524f292e3641feeb93989d9f8bf472e35bee8
Edilaine Santos <edilaine.santos839@gmail.com>
2023-07-21 20:41:08 -0300

    Add useState Comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,41 @@ import { Sidebar } from './components/Sidebar'
 import './global.css'
 import styles from './App.module.css'
 
+//author: {avatar_url: "", name: "", role: ""}
+//publishedAt: Date
+//content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/edilaine-as.png',
+      name: 'Edilaine Santos',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-07-21 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Olá pessoaal' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. O nome do projeto é IgniteFeed.'},
+      { type: 'link', content: 'jane.design/ignitefeed' },
+    ],
+    publishedAt: new Date('2023-07-22 20:00:00')
+  },
+];
+
 export function App() {
   return (
     <>
@@ -14,14 +49,14 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post
-            author="Edilaine"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eos omnis consequuntur quisquam magni ducimus alias ex soluta perspiciatis rem praesentium, possimus quos sequi saepe. Illum, molestiae! Quos, quam expedita."
-          />
-          <Post
-            author="Jonas"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse eos omnis consequuntur quisquam magni ducimus alias ex soluta perspiciatis rem praesentium, possimus quos sequi saepe. Illum, molestiae! Quos, quam expedita."
-          />
+          {posts.map(post => {
+            return (<Post 
+                      author={post.author}
+                      content={post.content}
+                      publishedAt={post.publishedAt}
+                   />
+            )
+          })}
         </main>
       </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,55 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
-import styles from './Post.module.css'
 
-export function Post(){
+import styles from './Post.module.css'
+import { useState } from 'react';
+
+export function Post( {author, publishedAt, content }){
+    const [comments, setComments] = useState([
+        1, 
+        2,
+    ])
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {locale: ptBR});
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
+    function handleCreateNewComment(){
+        event.preventDefault();
+        
+        //copia todos os comentarios que eu ja tenho usando o Spread Operator
+        setComments([...comments, comments.length + 1]);
+    }
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/edilaine-as.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Edilaine Santos</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title='19 de julho às 20:00h' dateTime='2023-07-19 20:00:00'>Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="">#novoprojeto</a>{' '}
-                    <a href="">#nlw</a>{' '}
-                    <a href="">#rocketseat</a>
-                </p>
+                {content.map(line => {
+                    if(line.type === 'paragraph'){
+                        return <p>{line.content}</p>;
+                    }else if(line.type === 'link'){
+                        return <p><a href="">{line.content}</a></p>;
+                    }
+                })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
@@ -41,7 +62,9 @@ export function Post(){
             </form>
 
             <div className={styles.commentList}>
-                <Comment/>
+                {comments.map(comment => {
+                    return <Comment/>;
+                })}
             </div>
         </article>
     )
