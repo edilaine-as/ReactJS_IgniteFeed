4ae2146fa4de049e1727767c72ebe230bf41d3da
Edilaine Santos <edilaine.santos839@gmail.com>
2023-07-23 11:31:43 -0300

    Setter New Comment Text

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,9 +9,12 @@ import { useState } from 'react';
 
 export function Post( {author, publishedAt, content }){
     const [comments, setComments] = useState([
-        1, 
-        2,
+        "Post muito boom!"
     ])
+
+    //é importante eu inicializar o estado do react com uma informação que tem o mesmo formato, o mesmo tipo da informação que vamos armazenar dps
+    //se eu armazenar um texto, estado é '', se eu armazenar um array de comentarios, [] etc...
+    const [newCommentText, setNewCommentText] = useState('')
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {locale: ptBR});
     const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
         locale: ptBR,
@@ -20,9 +23,14 @@ export function Post( {author, publishedAt, content }){
 
     function handleCreateNewComment(){
         event.preventDefault();
-        
+
         //copia todos os comentarios que eu ja tenho usando o Spread Operator
-        setComments([...comments, comments.length + 1]);
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange(){
+        setNewCommentText(event.target.value);
     }
 
     return (
@@ -53,7 +61,10 @@ export function Post( {author, publishedAt, content }){
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
+                    name='comment'
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -63,7 +74,7 @@ export function Post( {author, publishedAt, content }){
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment/>;
+                    return <Comment content={comment}/>;
                 })}
             </div>
         </article>
